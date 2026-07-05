be5286fc21aaadf9fab168af0176abdb6cb46aba
el-mahdi safouane <elmahdi.safouane@veltiston.ai>
2026-07-05 13:47:50 +0100

    finish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter, Playfair_Display } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
@@ -23,6 +24,17 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-PVMBJ8FWV2" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-PVMBJ8FWV2');
+          `}
+        </Script>
       </body>
     </html>
   );
